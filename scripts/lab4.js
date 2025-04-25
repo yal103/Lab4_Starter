4ef982542cac6c5d4e7811be0172b66810c8c0ab
yal103 <yal103@ucsd.edu>
2025-04-25 13:27:31 -0700

    fix functions

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,15 +6,25 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
+    // check if num1 and num2 are numbers
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return false;
+    }
+
+    // check if add is a boolean
+    if (typeof add !== 'boolean') {
+        return false;
+    }
+
     if (add) { 
-        const result = 0;
-        //  random comment to test pipeline
+        let result = 0;
+
         result = num1 + num2;
 
         return result;
     }
     else {
-        return !add;
+        return add;
     }
 }
 
@@ -25,11 +35,20 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
+    // validates prices and discount
+    if (!Array.isArray(prices) || prices.length === 0 || 
+        typeof discount !== 'number' || discount < 0 || discount > 1) {
+            return false;
+    }
     const discounted = []
     const length = prices.length;
     let discountedPrice = 0
     for(let i = 0; i < length; i++) {
-        discountedPrice += prices[i] * (1 - discount);
+        // check if price is number
+        if (typeof prices[i] !== 'number' || prices[i] < 0) {
+            return false;
+        }
+        discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
 
